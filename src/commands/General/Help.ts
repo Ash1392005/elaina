eaddc19f5034a03c8f4f12d8e92bdc512fb244e8
Ash1392005 <95693441+Ash1392005@users.noreply.github.com>
2022-03-31 12:18:01 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -22,7 +22,7 @@ export default class Command extends BaseCommand {
 	): Promise<void> => {
 		const user = M.sender.jid;
 		const chitoge =
-			"https://telegra.ph/file/db9ace10582e475ac78eb.mp4";
+			"./assets/Videos/Rin.mp4";
 		if (!parsedArgs.joined) {
 			const commands = this.handler.commands.keys();
 			const categories: { [key: string]: ICommand[] } = {};
