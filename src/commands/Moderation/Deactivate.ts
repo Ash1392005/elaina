ad086df674602b42bf58de9cdfbea83f66d71f02
Ash1392005 <95693441+Ash1392005@users.noreply.github.com>
2022-03-31 12:41:03 +0530

    Update Deactivate.ts

--- src/commands/Moderation/Deactivate.ts
+++ src/commands/Moderation/Deactivate.ts
@@ -1,56 +1,28 @@
-import MessageHandler from "../../Handlers/MessageHandler";
-import BaseCommand from "../../lib/BaseCommand";
-import WAClient, { toggleableGroupActions } from "../../lib/WAClient";
-import { IParsedArgs, ISimplifiedMessage } from "../../typings";
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient, { toggleableGroupActions } from '../../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
-  constructor(client: WAClient, handler: MessageHandler) {
-    super(client, handler, {
-      adminOnly: true,
-      command: "deactivate",
-      aliases: ["deact"],
-      description: "deactivate certain features on group-chats",
-      category: "moderation",
-      usage: `${client.config.prefix}deactivate [feature]`,
-      baseXp: 0,
-    });
-  }
-
-  run = async (
-    M: ISimplifiedMessage,
-    { joined }: IParsedArgs
-  ): Promise<void> => {
-    const type = joined.trim().toLowerCase() as toggleableGroupActions;
-    if (!Object.values(toggleableGroupActions).includes(type))
-      return void M.reply(
-        `🟥 Invalid Option: *${this.client.util.capitalize(type)}*`
-      );
-    const data = await this.client.getGroupData(M.from);
-    if (!data[type])
-      return void M.reply(
-        `🟨 *${this.client.util.capitalize(
-          type
-        )}* is already *inactived*, Baka!`
-      );
-    if (type === "news") {
-      await this.client.DB.group.updateOne(
-        { jid: M.from },
-        { $set: { news: false } }
-      );
-      await this.client.DB.feature.updateOne(
-        { feature: "news" },
-        { $pull: { jids: M.from } }
-      );
-      return void M.reply(
-        `🟩 *${this.client.util.capitalize(type)}* is now inactive`
-      );
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            adminOnly: true,
+            command: 'deactivate',
+            aliases: ['deact'],
+            description: 'deactivate certain features on group-chats',
+            category: 'moderation',
+            usage: `${client.config.prefix}deactivate [feature]`,
+            baseXp: 0
+        })
     }
-    await this.client.DB.group.updateOne(
-      { jid: M.from },
-      { $set: { [type]: false } }
-    );
-    return void M.reply(
-      `🟩 *${this.client.util.capitalize(type)}* is now inactive`
-    );
-  };
+
+    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
+        const type = joined.trim().toLowerCase() as toggleableGroupActions
+        if (!Object.values(toggleableGroupActions).includes(type))
+            return void M.reply(`🟥 𝕨𝕙𝕠 𝕞𝕒𝕕𝕖 𝕪𝕠𝕦 𝕒𝕕𝕞𝕚𝕟: *${this.client.util.capitalize(type)}*`)
+        const data = await this.client.getGroupData(M.from)
+        if (!data[type]) return void M.reply(`🟨 *${this.client.util.capitalize(type)}* 𝕛𝕖𝕣𝕜 𝕠𝕡𝕖𝕟 𝕪𝕠𝕦𝕣 𝕖𝕪𝕖𝕤 *inactived*, 𝕓𝕒𝕜𝕝𝕠𝕝!`)
+        await this.client.DB.group.updateOne({ jid: M.from }, { $set: { [type]: false } })
+        return void M.reply(`🟩 *${this.client.util.capitalize(type)}* 𝕚 𝕒𝕞 𝕘𝕠𝕚𝕟𝕘 𝕕𝕠𝕨𝕟 𝕕𝕠𝕨𝕟 𝕚𝕟𝕒𝕔𝕥𝕚𝕧𝕖`)
+    }
 }
