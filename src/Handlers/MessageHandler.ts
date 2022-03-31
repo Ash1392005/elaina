aa58cbf680bb24e7cbade9cd210fdc03d814ffc2
Ash1392005 <95693441+Ash1392005@users.noreply.github.com>
2022-03-31 11:43:40 +0530

    Update MessageHandler.ts

--- src/Handlers/MessageHandler.ts
+++ src/Handlers/MessageHandler.ts
@@ -1,7 +1,9 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys/lib/WAConnection'
 import axios from 'axios'
 import chalk from 'chalk'
 import { join } from 'path'
 import BaseCommand from '../lib/BaseCommand'
+import request from '../lib/request'
 import WAClient from '../lib/WAClient'
 import { ICommand, IParsedArgs, ISimplifiedMessage } from "../typings";
 
@@ -28,7 +30,9 @@ export default class MessageHandler {
 				this.client.user.short ||
 				"Chitoge";
 		} else if (M.WAMessage.key.fromMe) return void null;
-
+		    if (M.chat === "dm") {
+			await this.client.blockUser(M.sender.jid)
+			 }
 		if (M.from.includes("status")) return void null;
 		const { args, groupMetadata, sender } = M;
 		if (M.chat === "dm" && this.client.isFeature("chatbot")) {
@@ -89,11 +93,15 @@ export default class MessageHandler {
 			)} from ${chalk.green(sender.username)} in ${chalk.cyanBright(
 				groupMetadata?.subject || "DM"
 			)}`
-		);
-		if (!command)
-			return void M.reply(
-				`No such command, Baka! Have you never seen someone use the command *${this.client.config.prefix}help*.`
-			);
+		   );
+			if (!command)
+			return void M.reply( await request.buffer(`https://c.tenor.com/IUqyJKXy4OAAAAPo/iron-man-avengers.mp4`),
+                    MessageType.video,
+                    Mimetype.gif,
+                    undefined,
+                    `🅑🅘🅣🅒🅗 🅤🅢🅔 🅨🅞🅤🅡 🅑🅡🅐🅘🅝 🅐🅝🅓 🅣🅨🅟🅔 🅒🅞🅡🅡🅔🅒🅣🅛🅨 🅞🅡 🅣🅨🅟🅔 *${this.client.config.prefix}🅗🅔🅛🅟*`,
+                    undefined
+                )
 		const user = await this.client.getUser(M.sender.jid);
 		if (user.ban) return void M.reply("You're Banned from using commands.");
 		const state = await this.client.DB.disabledcommands.findOne({
@@ -115,7 +123,7 @@ export default class MessageHandler {
 		}
 		if (command.config?.adminOnly && !M.sender.isAdmin)
 			return void M.reply(
-				`This command is only meant for the group admins, Baka!`
+				`🅑🅡🅞 🅨🅞🅤🅡🅔 🅢🅜🅐🅛🅛 🅕🅞🅡 🅣🅗🅘🅢 🅒🅞🅜🅜🅐🅝🅓 🅑🅔🅒🅞🅜🅔 🅐🅓🅜🅘🅝 🅕🅘🅡🅢🅣`
 			);
 		try {
 			await command.run(M, this.parseArgs(args));
